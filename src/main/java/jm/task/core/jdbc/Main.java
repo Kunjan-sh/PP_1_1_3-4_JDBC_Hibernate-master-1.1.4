5b7a95f666be418d893add8c980bb369e8c8f94c
Kunjan Shrestha <139638441+Kunjan-sh@users.noreply.github.com>
2024-10-06 03:32:49 +0300

    fixed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,12 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Ivan", "Ivanov", (byte) 22);
         userService.saveUser("Nikolai", "Vladimirov", (byte) 30);
